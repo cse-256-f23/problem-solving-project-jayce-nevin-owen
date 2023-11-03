89562bb54b461ac27e7caeeeb4adf5f3d33769a2
Jayce Bordelon <jaycebordelon@Jayces-MBP.lan>
2023-11-03 15:54:14 -0500

    poop

--- view.js
+++ view.js
@@ -79,11 +79,43 @@ sidebar.append(ideas);
 let steps;
 switch (current_task) {
     case "remove_direct_permission":
-        steps = ["Click on the lock next to <strong>presentation_documents</strong>.", "Select 'Add...'", "Select 'employee4'", "Select 'Deny' for 'all_permissions'.", "Click ok."]
+        steps = ["Click on the lock next to <strong>presentation_documents</strong>.", "Select 'Add...'", "Select 'employee4'", "Select 'Ok'", "Click ok."]
         break;
     case "add_new_user":
-        steps = ["Click on the lock next to <strong>importantfile.txt</strong>.", "Select 'employee3'", "Remove all of their 'alllow' permissions.", "Select 'Deny' for 'all_permissions'.", "Click ok."]
+        steps = ["Click on the lock next to <strong>importantfile.txt</strong>.", "Select 'employee3'", "Remove all of their 'allow' permissions.", "Select 'Deny' for 'all_permissions'.", "Click ok."]
         break;
+    case "add_full_permissions":
+        steps = ["Click on the lock next to <strong>presentation documents</strong>.", "Select 'Add...'","Select 'new_manager'", "click ok", "click 'new manager' under the group or users names column",
+        "Look at the allow column and click the checkbox next to full_control", "Click ok."]
+        break;
+    case "remove_inherited_permission":
+        steps = ["Click on the lock next to <strong>importantfile.txt</strong>.","Select 'employee 3'", "click the check box that is next to modify <strong>that is also under the deny column</strong>", "Click ok."]
+        break;
+    case "intern_permissions":
+            steps = ["Click on the lock next to <strong>intern_subproject</strong>.", "Select 'intern'", "click the check box that is under the allow column that is also next to 'write'",
+            "Click ok."]
+            break;
+    case "remove_user_with_inheritance":
+        steps = ["Click on the lock next to <strong>importantfile.txt</strong>.", "Select 'employee 3'", "click the check box that is next to the full control column <strong>and is also under the deny column</strong>",  "Click ok."]
+        break;
+    case "restrict_group_member":
+        steps = ["Click on the lock next to <strong>importantfile.txt</strong>.",
+        "click 'advanced'",
+            "click edit",
+            "Click the change button.",
+            "Select 'employee 3'",
+        "click ok",
+        " <strong>(***CHECK THIS CAREFULLY***)</strong>Under the allow column, check every box that has the word 'read'",
+            "Under the deny column, check every box that was <strong>not</strong> checked on the left",
+            "Click ok.",
+            "close all the popups"]
+        break;
+        case "let_ta_modify":
+            steps = ["Click on the lock next to <strong>Lecture_notes</strong>.", "Click Advanced", "click the bottommost checkbox",  "Click yes", "Click ok.", "Close the popups."]
+            break;
+        case "lost_inheritance":
+            steps = ["Click on the lock next to <strong>Lecture_notes</strong>.", "Click Advanced", "click the bottommost checkbox",  "Click yes", "Click ok.", "Close the popups."]
+            break;
     default:
         break;
 }
